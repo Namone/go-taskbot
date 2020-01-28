4a957bc4e840592c10cc4abf031bec86b4a2041f
Namone <al2xnord@yahoo.com>
2020-01-27 21:04:39 -0700

    wip

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
 	"encoding/json"
-	"os"
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"os"
 
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
+	github "github.com/google/go-github/github"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 	githuboauth "golang.org/x/oauth2/github"
 )
 
@@ -17,36 +19,37 @@ const htmlIndex = `<html><body>
 Log in with <a href="/login">GitHub</a>
 </body></html>
 `
+
 var (
 	oauthConf = &oauth2.Config{
-			ClientID: os.Getenv("CLIENT_ID"),
-			ClientSecret: os.Getenv("CLIENT_SECRET"),
-			Scopes: []string{
-				"repo:status",
-				"repo_deployment",
-				"read:repo_hook",
-				"write:discussion",
-				"workflow",
-			},
-			Endpoint: githuboauth.Endpoint,
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		Scopes: []string{
+			"repo:status",
+			"repo_deployment",
+			"read:repo_hook",
+			"write:discussion",
+			"workflow",
+		},
+		Endpoint: githuboauth.Endpoint,
 	}
 	oauthStateString = "randomsalt"
 )
 
 func tokenToJSON(token *oauth2.Token) (string, error) {
-    if d, err := json.Marshal(token); err != nil {
-        return "", err
-    } else {
-        return string(d), nil
-    }
+	if d, err := json.Marshal(token); err != nil {
+		return "", err
+	} else {
+		return string(d), nil
+	}
 }
 
 func tokenFromJSON(jsonStr string) (*oauth2.Token, error) {
-    var token oauth2.Token
-    if err := json.Unmarshal([]byte(jsonStr), &token); err != nil {
-        return nil, err
-    }
-    return &token, nil
+	var token oauth2.Token
+	if err := json.Unmarshal([]byte(jsonStr), &token); err != nil {
+		return nil, err
+	}
+	return &token, nil
 }
 
 func handleMain(w http.ResponseWriter, r *http.Request) {
@@ -63,26 +66,26 @@ func handleGitHubLogin(w http.ResponseWriter, r *http.Request) {
 func handleGitHubCallback(w http.ResponseWriter, r *http.Request) {
 	state := r.FormValue("state")
 	if state != oauthStateString {
-			fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
+		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
 	}
 
 	code := r.FormValue("code")
 	token, err := oauthConf.Exchange(oauth2.NoContext, code)
 	if err != nil {
-			fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
+		fmt.Printf("oauthConf.Exchange() failed with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
 	}
 
 	oauthClient := oauthConf.Client(oauth2.NoContext, token)
 	client := github.NewClient(oauthClient)
 	user, _, err := client.Users.Get(oauth2.NoContext, "")
 	if err != nil {
-			fmt.Printf("client.Users.Get() faled with '%s'\n", err)
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-			return
+		fmt.Printf("client.Users.Get() faled with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
 	}
 	tokenAsString, err := tokenToJSON(token)
 	if err != nil {
@@ -96,41 +99,62 @@ func handleGitHubCallback(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 }
 
-type hook_struct struct {
-		Action string `json:"action"`
-		Number int `json:"number"`
-		PullRequest struct {
-			URL string `json:"url"`
-			Body string `json:"body"`
-			ClosedAt string `json:"closed_at"`
-		} `json:"pull_request"`
-		Description string `json:"description"`
-		Repository struct {
-			Name string `json:"name"`
-		} `json:"repository"`
+type pullRequestHook struct {
+	Action      string `json:"action"`
+	Number      int    `json:"number"`
+	PullRequest struct {
+		URL      string `json:"url"`
+		Body     string `json:"body"`
+		State    string `json:"state"`
+		ClosedAt string `json:"closed_at"`
+	} `json:"pull_request"`
+	Description string `json:"description"`
+	Repository  struct {
+		Name  string `json:"name"`
+		Owner struct {
+			Login string `json:"login"`
+		} `json:"owner"`
+	} `json:"repository"`
 }
 
 var client *github.Client
+var ctx = context.Background()
+var (
+	prSubject     = flag.String("pr-title", "test", "Title of the pull request. If not specified, no pull request will be created.")
+	prDescription = flag.String("pr-text", "testing", "Text to put in the description of the pull request.")
+)
+
 func handleWebhooks(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(body))
 
-	var hook hook_struct
+	var hook pullRequestHook
 	err = json.Unmarshal(body, &hook)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(hook.Description)
-	fmt.Println(hook.Repository)
-	fmt.Println(hook.Repository.Name)
+	newPR := &github.PullRequest{
+		Title:               prSubject,
+		Body:                flag.String("pr-body", hook.PullRequest.Body, "PR Body."),
+		State:               flag.String("pr-state", hook.PullRequest.State, "PR State."),
+		MaintainerCanModify: github.Bool(false),
+	}
+	if hook.Action == "opened" {
+		pr, _, err := client.PullRequests.Edit(
+			ctx,
+			hook.Repository.Owner.Login,
+			hook.Repository.Name,
+			hook.Number,
+			newPR)
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Println(hook.Action)
-	if hook.Action == "pull_request" {
-		//pr, _, err := client.PullRequests.Edit(ctx, *prRepoOwner, *prRepo, newPR)
+		fmt.Println(pr)
+		fmt.Println("Updated PR!")
 	}
 }
 
